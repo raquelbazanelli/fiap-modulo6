174db4acc077dfbeaf96d7b22b8971af6a19b4cb
raquelbazanelli <35847912+raquelbazanelli@users.noreply.github.com>
2021-04-19 23:28:29 -0300

    Update Cliente.java

--- src/main/java/com/fiap/creditcard/domain/Cliente.java
+++ src/main/java/com/fiap/creditcard/domain/Cliente.java
@@ -1,15 +1,21 @@
 package com.fiap.creditcard.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Cliente {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nome;
+	
+	public Cliente() {
+		
+	}
 	
 	public Cliente(Long id) {
 		super();
